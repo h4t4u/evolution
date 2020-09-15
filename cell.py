f584f9655d3a36b44c06f596b59dc62b9f703855
h4t4u <h4t4u.4ever@gmail.com>
2020-09-15 10:29:45 +0300

    codestyle patch

--- cell.py
+++ cell.py
@@ -4,7 +4,7 @@ width = 10
 height = 10
 
 class Cell:
-	def __init__(self, x, y, v, angle, r, interaction, feel_carni, feel_pred, m, limit):
+	def __init__(self, x, y, v, angle, r, interaction, feel_herb, feel_pred, m, limit, part):
 		self.x = x
 		self.y = y
 		self.v = v
@@ -15,80 +15,90 @@ class Cell:
 		self.interaction = interaction
 		self.m = m
 		self.limit = limit
-		self.feel_carni = feel_carni
+		self.feel_herb = feel_herb
 		self.feel_pred = feel_pred
-		self.sr=0
+		self.sr = 0
+		self.part = part
 
 	def create(self, part):
-		angle = random.randint(0,360)
-		r = int(abs(self.r+random.gauss(0,2)))
-		return Cell(int(self.x+(self.r+r)*1.5*math.cos(angle*3.14/180)), int(self.y+(self.r+r)*1.5*math.sin(angle*3.14/180)), self.v+random.gauss(0,5),
-					self.angle+random.gauss(0,5), r, self.interaction+random.gauss(0,10),
-					self.feel_carni+random.gauss(0,10), self.feel_pred+random.gauss(0,5), int(self.m*part), self.limit+random.gauss(0,10))
+		angle = random.randint(0, 360)
+		r = int(abs(self.r + random.gauss(0, 2)))
+		return Cell(int(self.x + 1.5*(self.r + r)*math.cos(angle * 3.14/180)), int(self.y + 1.5*(self.r + r)*math.sin(angle * 3.14/180)),
+						self.v + random.gauss(0, 5), self.angle + random.gauss(0, 5), r, self.interaction + random.gauss(0, 10),
+						self.feel_herb + random.gauss(0, 10), self.feel_pred + random.gauss(0, 5), 
+						int(self.m * part), self.limit + random.gauss(0, 10), abs((self.part + random.gauss(0, 0.1))%1))
 
 	def step(self, dt):
-		#+(self.v_x*self.v_x+self.v_y*self.v_y)/70000
 		self.sr = 0
-		if self.m <=1:
-			return -1
-		self.angle += random.uniform(-3,3)
-		self.x+=int(self.v*math.cos(self.angle*3.14/180)*dt)
-		self.y+=int(self.v*math.sin(self.angle*3.14/180)*dt)
+		if self.m <= 1:
+			return - 1
+		self.angle += random.uniform(-3, 3)
+		self.x += int(self.v * math.cos(self.angle * 3.14/180)*dt)
+		self.y += int(self.v * math.sin(self.angle * 3.14/180)*dt)
 		self.correct_coords()
 		self.get_mass()
 		self.lose_mass()
-		a = 3
-		if self.m>=self.limit :
-			self.m*=0.95
-			part = 0.5
+		if self.m >= self.limit :
+			self.m *= 0.95
+			part = self.part
 			child = self.create(part)
-			self.m = int(self.m*0.5)
+			self.m = int((1 - part) * self.m)
 			return child
 
 	def interact(self, other):
 		r = self.radius(other)
 		self.sr += r
 		if r < (self.r + other.r):
-			if self.interaction > 0 and other.m>0:
-				if other.m<self.interaction*5:
+			if self.interaction > 0 and other.m > 0:
+				mtp = 5
+				if other.m < self.interaction*mtp:
 					self.m += other.m
 					other.m = 0
 				else:
-					other.m -= self.interaction*5
-					self.m += self.interaction*5
-		dx = other.x-self.x
-		if abs(dx) > width-abs(dx):
-			dx = dx-width
+					other.m -= self.interaction*mtp
+					self.m += self.interaction*mtp
+		dx = other.x - self.x
+		if abs(dx) > width - abs(dx):
+			if dx > 0:
+				dx = dx - width
+			else:
+				dx = width + dx
 
-		dy = other.y-self.y
-		if abs(dy)>height-abs(dy):
-			dy = dy-height
-		mult = self.v*math.cos(self.angle*3.14/180)*(dy)-self.v*math.sin(self.angle*3.14/180)*(dx)
+		dy = other.y - self.y
+		if abs(dy) > height - abs(dy):
+			if dy > 0:
+				dy = dy - height
+			else:
+				dy = height + dy
+
+		mult = self.v * math.cos(self.angle * 3.14/180)*dy - self.v * math.sin(self.angle * 3.14/180)*dx
+
+		k = 1000
 		if other.interaction < 0:
 			if mult < 0:
-				self.angle-=200*self.feel_carni/((r+1)*(r+1))
+				self.angle -= k * self.feel_herb/((r+2) * (r+2))
 			else:
-				self.angle+=200*self.feel_carni/((r+1)*(r+1))
+				self.angle += k * self.feel_herb/((r+2) * (r+2))
 
 		if other.interaction > 0:
 			if mult < 0:
-				self.angle-=200*self.feel_pred/((r+1)*(r+1))
+				self.angle -= k * self.feel_pred/((r+2) * (r+2))
 			else:
-				self.angle+=200*self.feel_pred/((r+1)*(r+1))
+				self.angle += k * self.feel_pred/((r+2) * (r+2))
 
 		if self.interaction < 0:	
-			self.m -= 0.06*self.r * other.r/(2*r+1)
+			self.m -= 0.06 * self.r * other.r/(2*r + 1)
 			
 
 	def radius(self, other):
-		dx = abs(other.x-self.x)
-		if dx > width-dx:
-			dx = width-dx
+		dx = abs(other.x - self.x)
+		if dx > width - dx:
+			dx = width - dx
 
-		dy = abs(other.y-self.y)
-		if dy>height-dy:
-			dy = height-dy
-		return math.sqrt((dx)*(dx)+(dy)*(dy))
+		dy = abs(other.y - self.y)
+		if dy > height - dy:
+			dy = height - dy
+		return math.sqrt( dx * dx + dy * dy)
 
 	def correct_coords(self):
 		if self.x > width/2:
@@ -102,8 +112,8 @@ class Cell:
 			self.y = width/2
 
 	def get_mass(self):
-		if self.interaction<0:
-			self.m+=0.09*(self.r+3)
+		if self.interaction < 0:
+			self.m += 0.09 * (self.r+3)
 
 	def lose_mass(self):
-		self.m -= 0.08*(0.015*self.r+1 + abs(self.v*0.08))
+		self.m -= 0.08 * (0.015 * self.r + abs(self.v*0.08) + 1)
--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import sys
 
 pygame.init()
 
-width = 2000
+width = 4000
+height =4000
 cell.width = width
-height =2000
 cell.height = height
-scale = 0.5
+scale = 0.25
 dT = 20
 sc = pygame.display.set_mode((int(width*scale), int(height*scale)))
+
 RED = (225, 0, 0)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 GRAY = (127, 127, 127, 127)
 
-cells = [cell.Cell(0, 10, 60, 20, 40, 0, 0, 0, 1200, 200)]
+cells = [cell.Cell(0, 0, 100, 0, 60, 0, 0, 0, 2000, 300, 0.5)]
 
 while 1:
 	sc.fill(WHITE)
@@ -25,21 +26,23 @@ while 1:
 			pygame.quit()
 			sys.exit()
 
-	n_c = 0
+	part = 0
+	n_h = 0
 	n_p = 0
-	feel_c_c = 0
-	feel_c_p = 0
-	feel_p_c = 0
+	feel_h_h = 0
+	feel_h_p = 0
+	feel_p_h = 0
 	feel_p_p = 0
 	for cell in cells:
+		part += cell.part
 		if cell.interaction > 0:
 			n_p += 1
-			feel_p_c += cell.feel_carni
+			feel_p_h += cell.feel_herb
 			feel_p_p += cell.feel_pred
 		else:
-			n_c += 1
-			feel_c_c += cell.feel_carni
-			feel_c_p += cell.feel_pred
+			n_h += 1
+			feel_h_h += cell.feel_herb
+			feel_h_p += cell.feel_pred
 		child = cell.step(0.1)
 		for cell2 in cells:
 			if cell2 != cell:
@@ -49,20 +52,21 @@ while 1:
 			continue
 		if child != None:
 			cells.append(child)
-			if cell.m<=1:
+			if cell.m <= 1:
 				cells.remove(cell)
 		COLOUR = GREEN
 		if cell.interaction > 0:
 			COLOUR = RED
-		pygame.draw.circle(sc, COLOUR, (int((cell.x+int(width/2))*scale), int((cell.y+int(height/2))*scale)), int(cell.r*scale))
-		pygame.draw.line(sc, GRAY,	[int((cell.x+int(width/2)-int(cell.m/2))*scale), int((cell.y+int(height/2))*scale)],
-									[int((cell.x+int(width/2)+int(cell.m/2))*scale), int((cell.y+int(height/2))*scale)], 2 )
-	if n_c == 0:
-		n_c = 1
+		pygame.draw.circle(sc, COLOUR, (int((cell.x + int(width/2)) * scale), int((cell.y + int(height1.5*/2)) * scale)), int(cell.r * scale))
+		pygame.draw.line(sc, GRAY,	[int((cell.x + int(width/2) - int(cell.m/2)) * scale), int((cell.y + int(height/2)) * scale)],
+									[int((cell.x + int(width/2) + int(cell.m/2)) * scale), int((cell.y + int(height/2)) * scale)], 2 )
+	if n_h == 0:
+		n_h = 1
 	if n_p == 0:
 		n_p = 1
-	print("carni: c", int(feel_c_c/n_c), "p", int(feel_c_c/n_c))
-	print("preda: c", int(feel_p_c/n_p), "p", int(feel_p_p/n_p))
+	print("herb: h", int(feel_h_h/n_h), "p", int(feel_h_p/n_h))
+	print("pred: h", int(feel_p_h/n_p), "p", int(feel_p_p/n_p))
 	pygame.display.update()
-
+	if len(cells) == 0:
+		break
 	pygame.time.delay(dT)
